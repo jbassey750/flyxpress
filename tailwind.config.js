6e7a09ec4bf3c9d021a36fc88455f791b3966c12
jbassey750 <judebassey750@gmail.com>
2024-01-01 09:01:54 +0100

    first commit flyxpress

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: 'class',
+  // darkMode: ['class', '[data-mode="dark"]'],
+}
+
